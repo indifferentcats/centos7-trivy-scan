c791dcacdd93fc87740b2d03f30df819158c98fe
Charlie Todd <ctodd@latlogic.com>
2022-12-07 10:22:35 -0500

    support dynamic image names

--- build-and-scan.sh
+++ build-and-scan.sh
@@ -11,11 +11,11 @@ if [ $all_cmd_available -eq 0 ]; then
 	echo "FATAL: One or more required executables are not available.  Stopping."
 	exit 1
 fi
-
-docker build -t centos:7-$(date +%Y%m%d) .
+img="centos:7-$(date +%Y%m%d)"
+docker build -t $img .
 results="scan_results-$(date +%Y%m%d-%H%M%S).json"
 summary="scan_summary-hc-$(date +%Y%m%d-%H%M%S).json"
-trivy --security-checks vuln --format json image centos:7-20221130  > $results
+trivy --security-checks vuln --format json image $img  > $results
 cat $results | jq '[.Results[].Vulnerabilities[] | {"Severity":.Severity,"PkgID":.PkgID,"PublishedDate":.PublishedDate} | select(.Severity=="CRITICAL" or .Severity=="HIGH")]' > $summary
 echo "INFO: Detailed results are in $results"
 echo "INFO: Critical/High results are in $summary"
